b8c53e39cdf5c5253b68ac7a13797cc114e25bc5
Satak <iamthesatak@gmail.com>
2026-09-07 01:11:48 +0530

    Set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Track_my_train/',
   plugins: [react()],
   server: {
     port: 5173,
